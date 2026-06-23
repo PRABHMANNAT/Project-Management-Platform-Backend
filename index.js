9fe981d63bd9d6d166213e98e841269dd9b6a987
Prabhmannat <23bcb10048@cuchd.in>
2026-06-23 16:39:58 +0530

    move entry point to src/index.js, update gitignore

--- index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config({
-  path: "./.env",
-});
-let myusername = process.env.DB_USERNAME;
-console.log("value: ", myusername);
-
-console.log("start of backend project ");
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
-
+import express from "express";
 dotenv.config({
   path: "./.env",
 });
-let myusername = process.env.DB_USERNAME;
-console.log("value: ", myusername);
+const app = express();
+const port = process.env.PORT || 3000;
 
-console.log("start of backend project ");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/instagram", (req, res) => {
+  res.send("this is an Instagram page");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
